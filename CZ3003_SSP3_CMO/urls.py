e7dfa0c8db7f625fe81e94e6ec23709406a0aade
Andy Chong Chin Shin <andyccs@google.com>
2017-09-01 16:52:42 +0800

    Getting start guide and reformat code using pep8

--- CZ3003_SSP3_CMO/urls.py
+++ CZ3003_SSP3_CMO/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'CZ3003_SSP3_CMO.views.home', name='home'),
     # url(r'^CZ3003_SSP3_CMO/', include('CZ3003_SSP3_CMO.foo.urls')),
@@ -14,5 +15,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', 'myapp.views.hello', name = 'hello'),
-)
+    url(r'^', 'myapp.views.hello', name='hello'), )
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,4 @@
-from django.db import models
+# TODO: uncomment the following import when it is used.
+# from django.db import models
 
 # Create your models here.
