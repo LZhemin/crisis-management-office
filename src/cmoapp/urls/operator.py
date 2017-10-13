10e01dcd10be876f35de3eb9883625323729669e
LeoQ <leoqys@gmail.com>
2017-10-13 16:45:33 +0800

    Small update

--- src/cmoapp/urls/operator.py
+++ src/cmoapp/urls/operator.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from cmoapp.views import OperatorManager
 
 urlpatterns = [
-    url(r'^', OperatorManager.index, name="Operator_Index")
+    url(r'^', OperatorManager.index, name="Operator_Index"),
+    url(r'^post/(?P<pk>\d+)/$', OperatorManager.index, name='post_detail')
 ]
