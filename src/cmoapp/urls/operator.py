81e6b11185ffc30e0cf2807e737889b7a72f367b
TeaEvoz-PC\TeaEvoz <teaevo@hotmail.com>
2017-10-16 09:38:41 +0800

    Updated some of the operator.htmls but still can't save the post

--- src/cmoapp/urls/operator.py
+++ src/cmoapp/urls/operator.py
@@ -2,7 +2,15 @@ from django.conf.urls import url
 from cmoapp.views import OperatorManager
 
 urlpatterns = [
-   url(r'^(?P<pk>\d+)', OperatorManager.viewCrisis, name='Operator_View'),
+    url(r'^(?P<pk>\d+)/$', OperatorManager.viewCrisis, name='Operator_View'),
+
+    url(r'^allolist/$', OperatorManager.getCrisisAllocationList, name='Operator_ViewAllocationList'),
+    url(r'^alloexcrisis/$', OperatorManager.allocateToExistingCrisis, name='Operator_allocateToExistingCrisis'),
+    url(r'^allocrisis/$', OperatorManager.allocateCrisis, name='Operator_AllocateCrisis'),
+    url(r'^newcrisisrpt/$', OperatorManager.newCrisisReport, name='Operator_NewCrisisReport'),
+
    #url(r'^(?P<pk>[0-9]+)/', OperatorManager.viewCrisis, name = "View_Crisis"),
     url(r'^$', OperatorManager.index, name="Operator_Index")
+
+
 ]
--- src/cmoapp/views/OperatorManager.py
+++ src/cmoapp/views/OperatorManager.py
@@ -7,79 +7,151 @@ from cmoapp.models import Account, Crisis, CrisisReport, CrisisType, Location, A
 
 def index(Request):
 
-    getallcrisis = CrisisReport.objects.all();
-    return render(Request, 'operator/index.html',{'getallcrisis':getallcrisis})
+    getallcrisis = CrisisReport.objects.order_by('-datetime')[:5]
+    context = {'getallcrisis': getallcrisis}
+    return render(Request, 'operator/index.html',
+                context,
+                {'error_message': "You didn't select a Crisis."}
+                )
 
 def getCrisisAllocationList(Request):
-    #latest_crisis_list = Crisis.objects.order_by('-datetime')[:5]
-    # output = ', '.join([l.Location for l in latest_crisis_list])
-    #context = {'latest_crisis_list': latest_crisis_list}
 
-    return render(Request, 'operator/base_site.html', {})
+    getAllocationList = Crisis.objects.all
+    context = {'getAllocationList': getAllocationList}
+    return render(Request, 'operator/allolist.html',
+                  context,
+                  {'error_message': "You didn't select a Crisis."}
+                  )
 
 def viewCrisis(Request, pk):
     #change = CrisisReport.objects.get(id=pk)
     getallcrisis = CrisisReport.objects.all()
     return render(Request, 'operator/assigncrisis.html', {'getallcrisis':getallcrisis})
 
-def allocateToExistingCrisis(request, crisis_id):
-    crisis_list = CrisisReport.objects.order_by('-datetime')
-    # output = ', '.join([l.Location for l in latest_crisis_list])
-    context = {'latest_crisis_list': crisis_list}
+
+def allocateToExistingCrisis(request):
+    getCrisisList = Crisis.objects.all
+    getCrisisTypeList = CrisisType.objects.all
+    getAccountList = Account.objects.all
+    context = {'getCrisisList': getCrisisList,
+               'getCrisisTypeList': getCrisisTypeList,
+               'getAccountList': getAccountList}
+    # getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
 
     try:
-        forCrisis = request.POST['CrisisReport']
-        selectedCrisis = Crisis.Crisis.get(forCrisis)
-    except(KeyError, selectedCrisis.DoesNotExist):
-    # Redisplay
-        return render(request, 'operator/base_site.html', {
-            context,
-            {'error_message': "You didn't select a Crisis."}
-        })
+        # selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
+        # selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
+        selected_analyst = request.POST['getanalyst']
+        selected_crisistype = request.POST['getcrisistype']
+
+
+    except(KeyError, Crisis.DoesNotExist):
+        context = {'getCrisisList': getCrisisList,
+                   'getCrisisTypeList': getCrisisTypeList,
+                   'getAccountList': getAccountList}
+
+        return render(request, 'operator/allocrisis.html',
+                      context,
+                      {'error_message': "You didn't select a Crisis."}
+                      )
 
     else:
-        crisisList = CrisisReport(crisis_id=forCrisis)
-        crisisList.save()  # save to database
-        return HttpResponseRedirect(reverse('cmoapp:base_site', args=(crisis_id)))
+        created_crisis = Crisis(analyst=selected_analyst, crisistypes=selected_crisistype)
+        created_crisis.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('operator/allolist.html'))
 
 
-def allocateCrisis(request, analyst_id):
-    latest_crisis_list = Crisis.objects.order_by('-datetime')[:5]
-    # output = ', '.join([l.Location for l in latest_crisis_list])
-    context = {'latest_crisis_list': latest_crisis_list}
+def allocateCrisis(request):
+    getCrisisList = Crisis.objects.all
+    getCrisisTypeList = CrisisType.objects.all
+    getAccountList = Account.objects.all
+    context = {'getCrisisList': getCrisisList,
+               'getCrisisTypeList': getCrisisTypeList,
+               'getAccountList': getAccountList}
+    #getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
 
     try:
-        forCrisis = request.POST['Crisis']
-        selectedCrisis = Crisis.Crisis.get(forCrisis)
-    except(KeyError, selectedCrisis.DoesNotExist):
-    # Redisplay
-        return render(request, 'operator/base_site.html', {
-            context,
-            {'error_message': "You didn't select a Crisis."}
-        })
+        #selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
+        #selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
+        selected_analyst = request.POST['getanalyst']
+        selected_crisistype = request.POST['getcrisistype']
+
+
+    except(KeyError, Crisis.DoesNotExist):
+        context = {'getCrisisList': getCrisisList,
+                   'getCrisisTypeList': getCrisisTypeList,
+                   'getAccountList': getAccountList}
+
+        return render(request, 'operator/allocrisis.html',
+                      context,
+                      {'error_message': "You didn't select a Crisis."}
+                      )
 
     else:
-        crisisList = Crisis(crisis_id=forCrisis)
-        crisisList.save()  # save to database
-        return HttpResponseRedirect(reverse('cmoapp:base_site', args=(analyst_id)))
+        created_crisis = Crisis(analyst=selected_analyst, crisistypes=selected_crisistype)
+        created_crisis.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('operator/allolist.html'))
 
 
-def newCrisisReport(request,Crisis_ID):
-    latest_crisis_list = CrisisReport.objects.order_by('-datetime')[:5]
-    # output = ', '.join([l.Location for l in latest_crisis_list])
-    context = {'latest_crisis_list': latest_crisis_list}
+
+
+
+    # latest_crisis_list = Crisis.objects.order_by('-datetime')[:5]
+    # # output = ', '.join([l.Location for l in latest_crisis_list])
+    # context = {'latest_crisis_list': latest_crisis_list}
+    #
+    # try:
+    #     forCrisis = request.POST['Crisis']
+    #     selectedCrisis = Crisis.Crisis.get(forCrisis)
+    # except(KeyError, selectedCrisis.DoesNotExist):
+    # # Redisplay
+    #     return render(request, 'operator/base_site.html', {
+    #         context,
+    #         {'error_message': "You didn't select a Crisis."}
+    #     })
+    #
+    # else:
+    #     crisisList = Crisis(crisis_id=forCrisis)
+    #     crisisList.save()  # save to database
+    #     return HttpResponseRedirect(reverse('cmoapp:base_site', args=(analyst_id)))
+
+
+def newCrisisReport(request):
+    getCrisisReportList = CrisisReport.objects.all
+    getCrisisTypeList = CrisisType.objects.all
+    getAccountList = Account.objects.all
+    context = {'getCrisisReportList': getCrisisReportList,
+               'getCrisisTypeList': getCrisisTypeList,
+               'getAccountList': getAccountList}
+    # getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
 
     try:
-        forCrisis = request.POST['Crisis']
-        selectedCrisis = Crisis.Crisis.get(forCrisis)
-    except(KeyError, selectedCrisis.DoesNotExist):
-    # Redisplay
-        return render(request, 'operator/base_site.html', {
-            context,
-            {'error_message': "You didn't select a Crisis."}
-        })
+        # selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
+        # selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
+        selected_analyst = request.POST['getanalyst']
+        selected_crisistype = request.POST['getcrisistype']
+
+
+    except(KeyError, Crisis.DoesNotExist):
+        context = {'getCrisisReportList': getCrisisReportList,
+                   'getCrisisTypeList': getCrisisTypeList,
+                   'getAccountList': getAccountList}
+
+        return render(request, 'operator/newcrisisrpt.html',
+                      context,
+                      {'error_message': "You didn't select a Crisis."}
+                      )
 
     else:
-        crisisList = CrisisReport(crisis_id=forCrisis)
-        crisisList.add()  # save to database
-        return HttpResponseRedirect(reverse('cmoapp:base_site', args=(Crisis_ID)))
+        created_crisis = Crisis(analyst=selected_analyst, crisistypes=selected_crisistype)
+        created_crisis.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('operator/allolist.html'))
