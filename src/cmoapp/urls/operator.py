7bdb986c0a1104c25a4b6721e6a2916782352f98
TeaEvoz-PC\TeaEvoz <teaevo@hotmail.com>
2017-10-24 00:11:39 +0800

    Updated the operator html

--- src/cmoapp/urls/operator.py
+++ src/cmoapp/urls/operator.py
@@ -2,15 +2,8 @@ from django.conf.urls import url
 from cmoapp.views import OperatorManager, ApiManager
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)', OperatorManager.assignnewCrisis, name='Operator_AssignCrisis'),
-
-    url(r'^allolist/$', OperatorManager.getCrisisAllocationList, name='Operator_ViewAllocationList'),
-    url(r'^alloexcrisis/$', OperatorManager.allocateToExistingCrisis, name='Operator_allocateToExistingCrisis'),
-    url(r'^allocrisis/$', OperatorManager.allocateCrisis, name='Operator_AllocateCrisis'),
-    url(r'^newcrisisrpt/$', OperatorManager.newCrisisReport, name='Operator_NewCrisisReport'),
-
-   #url(r'^(?P<pk>[0-9]+)/', OperatorManager.viewCrisis, name = "View_Crisis"),
     url(r'^$', OperatorManager.index, name="Operator_Index"),
+    url(r'^(?P<pk>\d+)', OperatorManager.assignnewCrisis, name='Operator_AssignCrisis'),
     url(r'^create_crisis/$', OperatorManager.create_crisis, name="create_crisis"),
     url(r'^delete_crisis/$', OperatorManager.delete_crisis, name="delete_crisis"),
 
--- src/cmoapp/views/OperatorManager.py
+++ src/cmoapp/views/OperatorManager.py
@@ -10,20 +10,27 @@ import json
 
 
 def sharedindex():
-    getunassignedCrisis = CrisisReport.objects.filter(crisis__isnull=True).order_by('datetime')
     getCrisisList = Crisis.objects.all
     getCrisisTypeList = CrisisType.objects.all
     getCrisisReportList = CrisisReport.objects.all
+    getUnassignedCrisisReport = CrisisReport.objects.filter(crisis__isnull=True).order_by('datetime')
+
+    #getanalystacc = Crisis.objects.filter(analyst__isnull=False)
+    #getAccountList = Account.objects.exclude(pk__in=getanalystacc).filter(type="Analyst")
+
+    getcrisisacc = CrisisReport.objects.filter(crisis__isnull=False)
+    getUnassignedCrisis = Crisis.objects.all().exclude(pk__in=getcrisisacc)
 
     getanalystacc = Crisis.objects.all().values_list('analyst_id', flat=True)
     getAccountList = Account.objects.exclude(pk__in=getanalystacc).filter(type="Analyst")
-    #getAccountList = Account.objects.filter(type='Analyst')
 
     context = {'getCrisisList': getCrisisList,
                'getCrisisTypeList': getCrisisTypeList,
                'getCrisisReportList': getCrisisReportList,
+               'getUnassignedCrisisReport': getUnassignedCrisisReport,
                'getAccountList': getAccountList,
-                'getunassignedCrisis': getunassignedCrisis,
+               'getUnassignedCrisis': getUnassignedCrisis,
+
                'all_crisis': Crisis.objects.reverse(),
                'all_crisisreport': CrisisReport.objects.reverse(),
                'form': CrisisForm()
@@ -110,123 +117,129 @@ def delete_crisis(request):
     else:
         return JsonResponse(model_to_dict(0))
 
-def getCrisisAllocationList(Request):
-
-    getAllocationList = Crisis.objects.all
-    context = {'getAllocationList': getAllocationList}
-    return render(Request, 'operator/allolist.html',
-                  context,
-                  {'error_message': "You didn't select a Crisis."}
-                  )
 
 
 
-def allocateToExistingCrisis(request):
-    getCrisisList = Crisis.objects.all
-    getCrisisTypeList = CrisisType.objects.all
-    getAccountList = Account.objects.all
-    context = {'getCrisisList': getCrisisList,
-               'getCrisisTypeList': getCrisisTypeList,
-               'getAccountList': getAccountList}
-    # getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
-
-    try:
-        # selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
-        # selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
-        selected_analyst = request.POST['getanalyst']
-        selected_crisistype = request.POST['getcrisistype']
 
 
-    except(KeyError, Crisis.DoesNotExist):
-        context = {'getCrisisList': getCrisisList,
-                   'getCrisisTypeList': getCrisisTypeList,
-                   'getAccountList': getAccountList}
 
-        return render(request, 'operator/allocrisis.html',
-                      context,
-                      {'error_message': "You didn't select a Crisis."}
-                      )
-
-    else:
-        created_crisis = Crisis(analyst=selected_analyst, crisistypes=selected_crisistype)
-        created_crisis.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('Operator_allocateToExistingCrisis'))
-
-
-def allocateCrisis(request):
-    getCrisisList = Crisis.objects.all
-    getCrisisTypeList = CrisisType.objects.all
-    getAccountList = Account.objects.filter(type = 'Analyst')
-    context = {'getCrisisList': getCrisisList,
-               'getCrisisTypeList': getCrisisTypeList,
-               'getAccountList': getAccountList}
-    #getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
-
-    try:
-        #selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
-        #selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
-        selected_analyst = request.POST["getanalyst"]
-        selected_crisistype = request.POST["getcrisistype"]
-        #get all crisis types types
-        selected_filtercrisistype = CrisisType.objects.filter(name = selected_crisistype)
-
-    except(KeyError, Crisis.DoesNotExist):
-        context = {'getCrisisList': getCrisisList,
-                   'getCrisisTypeList': getCrisisTypeList,
-                   'getAccountList': getAccountList}
-
-        return render(request, 'operator/allocrisis.html',
-                      context,
-                      {'error_message': "You didn't select a Crisis."}
-                      )
-
-    else:
-        created_crisis = Crisis(analyst_id=selected_analyst)
-        created_crisis.save()
-        created_crisis.crisistypes.add(selected_crisistype)
-
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('Operator_AllocateCrisis'))
-
-
-def newCrisisReport(request):
-    getCrisisReportList = CrisisReport.objects.all
-    getCrisisTypeList = CrisisType.objects.all
-    getAccountList = Account.objects.all
-    context = {'getCrisisReportList': getCrisisReportList,
-               'getCrisisTypeList': getCrisisTypeList,
-               'getAccountList': getAccountList}
-    # getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
-
-    try:
-        # selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
-        # selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
-        selected_crisisname = request.POST['getcrisisname']
-        selected_crisisdes = request.POST['getcrisisdes']
-        selected_crisislong = request.POST['getcrisislong']
-        selected_crisislat = request.POST['getcrisislat']
-        selected_crisisdatetime = request.POST['getcrisisdatetime']
-
-    except(KeyError, Crisis.DoesNotExist):
-        context = {'getCrisisReportList': getCrisisReportList,
-                   'getCrisisTypeList': getCrisisTypeList,
-                   'getAccountList': getAccountList}
-
-        return render(request, 'operator/newcrisisrpt.html',
-                      context,
-                      {'error_message': "You didn't select a Crisis."}
-                      )
-
-    else:
-        created_crisisrpt = CrisisReport(latitude=selected_crisislat, longitude=selected_crisislong
-                                      ,description=selected_crisisdes, datetime=selected_crisisdatetime)
-        created_crisisrpt.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('Operator_NewCrisisReport'))
+# def getCrisisAllocationList(Request):
+#
+#     getAllocationList = Crisis.objects.all
+#     context = {'getAllocationList': getAllocationList}
+#     return render(Request, 'operator/allolist.html',
+#                   context,
+#                   {'error_message': "You didn't select a Crisis."}
+#                   )
+#
+#
+#
+# def allocateToExistingCrisis(request):
+#     getCrisisList = Crisis.objects.all
+#     getCrisisTypeList = CrisisType.objects.all
+#     getAccountList = Account.objects.all
+#     context = {'getCrisisList': getCrisisList,
+#                'getCrisisTypeList': getCrisisTypeList,
+#                'getAccountList': getAccountList}
+#     # getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
+#
+#     try:
+#         # selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
+#         # selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
+#         selected_analyst = request.POST['getanalyst']
+#         selected_crisistype = request.POST['getcrisistype']
+#
+#
+#     except(KeyError, Crisis.DoesNotExist):
+#         context = {'getCrisisList': getCrisisList,
+#                    'getCrisisTypeList': getCrisisTypeList,
+#                    'getAccountList': getAccountList}
+#
+#         return render(request, 'operator/allocrisis.html',
+#                       context,
+#                       {'error_message': "You didn't select a Crisis."}
+#                       )
+#
+#     else:
+#         created_crisis = Crisis(analyst=selected_analyst, crisistypes=selected_crisistype)
+#         created_crisis.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('Operator_allocateToExistingCrisis'))
+#
+#
+# def allocateCrisis(request):
+#     getCrisisList = Crisis.objects.all
+#     getCrisisTypeList = CrisisType.objects.all
+#     getAccountList = Account.objects.filter(type = 'Analyst')
+#     context = {'getCrisisList': getCrisisList,
+#                'getCrisisTypeList': getCrisisTypeList,
+#                'getAccountList': getAccountList}
+#     #getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
+#
+#     try:
+#         #selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
+#         #selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
+#         selected_analyst = request.POST["getanalyst"]
+#         selected_crisistype = request.POST["getcrisistype"]
+#         #get all crisis types types
+#         selected_filtercrisistype = CrisisType.objects.filter(name = selected_crisistype)
+#
+#     except(KeyError, Crisis.DoesNotExist):
+#         context = {'getCrisisList': getCrisisList,
+#                    'getCrisisTypeList': getCrisisTypeList,
+#                    'getAccountList': getAccountList}
+#
+#         return render(request, 'operator/allocrisis.html',
+#                       context,
+#                       {'error_message': "You didn't select a Crisis."}
+#                       )
+#
+#     else:
+#         created_crisis = Crisis(analyst_id=selected_analyst)
+#         created_crisis.save()
+#         created_crisis.crisistypes.add(selected_crisistype)
+#
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('Operator_AllocateCrisis'))
+#
+#
+# def newCrisisReport(request):
+#     getCrisisReportList = CrisisReport.objects.all
+#     getCrisisTypeList = CrisisType.objects.all
+#     getAccountList = Account.objects.all
+#     context = {'getCrisisReportList': getCrisisReportList,
+#                'getCrisisTypeList': getCrisisTypeList,
+#                'getAccountList': getAccountList}
+#     # getCrisisList = get_object_or_404(Crisis, pk=crisis_id)
+#
+#     try:
+#         # selected_crisis = getCrisisList.objects.all.analyst.get(request.POST['getanalyst'])
+#         # selected_crisis = getCrisisList.objects.all.crisistypes.get(request.POST['getcrisistype'])
+#         selected_crisisname = request.POST['getcrisisname']
+#         selected_crisisdes = request.POST['getcrisisdes']
+#         selected_crisislong = request.POST['getcrisislong']
+#         selected_crisislat = request.POST['getcrisislat']
+#         selected_crisisdatetime = request.POST['getcrisisdatetime']
+#
+#     except(KeyError, Crisis.DoesNotExist):
+#         context = {'getCrisisReportList': getCrisisReportList,
+#                    'getCrisisTypeList': getCrisisTypeList,
+#                    'getAccountList': getAccountList}
+#
+#         return render(request, 'operator/newcrisisrpt.html',
+#                       context,
+#                       {'error_message': "You didn't select a Crisis."}
+#                       )
+#
+#     else:
+#         created_crisisrpt = CrisisReport(latitude=selected_crisislat, longitude=selected_crisislong
+#                                       ,description=selected_crisisdes, datetime=selected_crisisdatetime)
+#         created_crisisrpt.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('Operator_NewCrisisReport'))
