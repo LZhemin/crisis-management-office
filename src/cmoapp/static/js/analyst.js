1be74f920b235d00dacb98ec320d5aaa2e6df561
Seng Cheong <sengcheong94@gmail.com>
2017-11-03 10:01:23 +0800

    Rearranged analyst JS codes

--- src/cmoapp/static/js/analyst.js
+++ src/cmoapp/static/js/analyst.js
@@ -1,14 +1,6 @@
 //Variables to track number of each of the stats
 var efCount=-1, commentCount=-1, crCount=-1;
 
-//Auto Update After 30 Seconds
-setInterval(function()
-{
-    checkEfUpdate();
-    checkCommentUpdate();
-    checkCRUpdate();
-}, 10000);
-
 //Used to Get the CSRF Token later on
 function getCookie(name) {
     var cookieValue = null;
@@ -33,6 +25,14 @@ $.ajaxSetup({
     }
 });
 
+/*
+function queryStatus()
+{
+    $.ajax({
+
+    });
+}
+*/
 
 //Polling for Updates Sections
 //Checking for New EFUpdates
@@ -214,53 +214,6 @@ function reloadCr(count){
     });
 }
 //--------------------------------------------
-
-$(document).ready(function() {
-    //Collapse to right code
-    $('.collapse-link-right').on('click', function () {
-        var $BOX_PANEL = $(this).closest('.x_content'),
-            $TOGGLE_PANEL = $($(this).attr('id')),
-            $OWN_PANEL = $('#own'),
-            $ICON = $(this).find('i');
-
-        $ICON.toggleClass("fa-chevron-left fa-chevron-right");
-        if($TOGGLE_PANEL.is(":visible")){
-            $TOGGLE_PANEL.addClass('fadeOutRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-                $OWN_PANEL.toggleClass('col-sm-9 col-sm-12');
-                $TOGGLE_PANEL.removeClass('fadeOutRight animated');
-                $TOGGLE_PANEL.hide();
-            });
-
-        }
-        else {
-            $OWN_PANEL.toggleClass('col-sm-9 col-sm-12');
-            $TOGGLE_PANEL.delay(500).show(0).addClass('fadeInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-                $TOGGLE_PANEL.removeClass('fadeInRight animated');
-            });
-        }
-
-        /*
-        if(!$BOX_TITLE.find('h2:eq(1)').is(':visible')){
-            console.log( $BOX_TITLE.find('h2:eq(1)').html());
-            $BOX_TITLE.find('h2:eq(1)').show();
-            $BOX_CONTENT.show();
-        }
-        else{
-            console.log( $BOX_TITLE.find('h2:eq(1)').html());
-            $BOX_TITLE.find('h2:eq(1)').hide();
-            $BOX_CONTENT.hide();
-        }
-        if ($BOX_TITLE.find('h2').attr('style')) {
-            $BOX_TITLE.find('h2').removeAttr('style');
-            $BOX_TITLE.find('h2').toggleClass('.collapse-right')
-        } else {
-            $BOX_CONTENT.slideToggle(200);
-            $BOX_PANEL.css('height', 'auto');
-        }
-
-        $ICON.toggleClass('fa-chevron-right fa-chevron-left');*/
-    });
-});
 
 /* Basic Templating JS */
 function attachDataToTemplate(template, data)
