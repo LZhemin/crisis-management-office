2a01691ab2e1f2d09f0ba2b0f5d07ca320260e32
Liu Zhemin <zhemin_liu@hotmail.com>
2017-11-10 02:57:37 +0800

    add poll technique for front end

--- src/cmoapp/static/js/chief.js
+++ src/cmoapp/static/js/chief.js
@@ -258,6 +258,15 @@ setInterval(function()
 
 }, 30000);
 
+//Auto Update After 3 Seconds
+setInterval(function()
+{
+    reload_notifications()
+
+
+}, 3000);
+
+
 //Checking for New EFUpdates
 function checkEfUpdate(){
     $.ajax({
@@ -341,6 +350,54 @@ function reload_crisis() {
         }
     });
 }
+
+
+//reloads the all_crisis template
+function reload_notifications() {
+    $.ajax({
+        url :"/chief/reload_notification/",
+        type : "GET", // http method
+        // handle a successful response
+        //var html;
+        success : function(data) {
+            if(!$('#presentation1').hasClass('open')) {
+                $('#chief_notifications').text(data.length);
+                $('#menu1').empty();
+                var i;
+                for (i = 0; i < data.length; ++i) {
+                    $('#menu1').append("<li><span><strong>" + data[i].title + "</strong></span>" +
+                        "</span><br/><span class='message'>" + data[i].text + "</span>" +
+                        "<span class='time text-right'>" + data[i].time_added + "</span></li>");
+                }
+            }
+        },
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+}
+
+
+$('#presentation1').on('click', function () {
+    if(!$('#presentation1').hasClass('open')) {
+        $.ajax({
+        url :"/chief/delete_notification/",
+        type : "GET", // http method
+        // handle a successful response
+        //var html;
+        success : function(data) {
+            console.log('Delete Notification Success')
+        },
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+    }else{
+        reload_notifications();
+    }
+});
 
 
 function select_crisischat(id) {
