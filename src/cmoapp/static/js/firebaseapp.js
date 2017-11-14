47513a3eb447e2e4d6a50b4f1e6746df5c1ff67e
Saklani Pankaj <drag0n_freak@hotmail.com>
2017-11-14 14:04:24 +0800

    Updates Firebase for EF Working

--- src/cmoapp/static/js/firebaseapp.js
+++ src/cmoapp/static/js/firebaseapp.js
@@ -1,25 +1,27 @@
 
 //PMO SECTION
 var app = angular.module('chatApp', ['firebase']);
-var ref, crisisChatID;
+var ref, crisisChatID, destination
 app.controller('ChatController', function($scope, $firebaseArray) {
         //Crisis
         //crisisChatID = "1";
         crisisChatID = 1;
+        destination = 'CMO-PMO';
         //$scope.crisis = ["1", "2", "3"];
-        ref = firebase.database().ref().child(crisisChatID).child('CMO-PMO');
+        ref = firebase.database().ref().child(crisisChatID).child(destination);
         scope = $scope;
         $scope.messages = $firebaseArray(ref);
 
 
         $scope.assignCrisis = function() {
             crisisChatID = $scope.crisisID;
-            ref = firebase.database().ref().child(crisisChatID).child('CMO-PMO');
+            ref = firebase.database().ref().child(crisisChatID).child(destination);
             $scope.messages = $firebaseArray(ref);
 
         }
+
         $scope.changeChat = function(){
-            ref = firebase.database().ref().child(crisisChatID).child('CMO-PMO');
+            ref = firebase.database().ref().child(crisisChatID).child(destination);
             $scope.messages = $firebaseArray(ref);
         }
 
@@ -34,6 +36,23 @@ app.controller('ChatController', function($scope, $firebaseArray) {
 function changeChat(id){
     crisisChatID = id;
     console.log(crisisChatID)
+    var scope = angular.element(document.getElementById('ChatController')).scope();
+    scope.$apply(function(){
+        scope.changeChat();
+    });
+}
+
+
+function changeDest(id){
+    console.log(document.getElementById('chatTitle'));
+    if(id=='CMO'){
+        destination='CMO-PMO';
+        document.getElementById('chatTitle').innerHTML = "<i class=\"fa fa-comments\"></i> PMO Chat";
+    }
+    else{
+       destination='CMO-EF';
+       document.getElementById('chatTitle').innerHTML = "<i class=\"fa fa-comments\"></i> EF Chat";
+    }
     var scope = angular.element(document.getElementById('ChatController')).scope();
     scope.$apply(function(){
         scope.changeChat();
